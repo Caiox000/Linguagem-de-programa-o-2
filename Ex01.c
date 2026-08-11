f6b6b4412f2e29087be324f3892303d91babd588
Caiox000 <caio.fecap@gmail.com>
2026-08-11 13:32:45 +0000

    Ex01

---
+++ Ex01.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+  printf("=============================\n");
+  printf("     BEM-VINDO AO LAB 01!\n");
+  printf("=============================\n\n");
+  printf("Seu Nome: Caio Ferradas\n");
+  printf("Data: 10/08/2026\n");
+  printf("Linguagem: c\n\n");
+  printf("Vamos aprender C juntos!\n");
+  printf("=============================\n");
+}
--- aula1.c
+++ aula1.c
@@ -4,7 +4,7 @@
 
 int main(){
   printf("Mackenzie - FCI - 1970 \n");
-  printf("Ciencia da computacao\n ");
+  printf("Ciencia da computacao\n");
   printf("Algoritmo e Prog. II\n");
 
   return 0;
